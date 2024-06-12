6a8150b423fd5fbd3be65ff47dd09ab7ce456199
plantainbread <119023568+plantainbread@users.noreply.github.com>
2024-06-12 09:31:38 -0700

    Update run.py

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from app.discord_bot.discord_api import client, discord_token
 if __name__ == '__main__':
     client.run(discord_token)
 
-#problem is that it doesn't let me import my own modules from the left, but the external ones work just fine.
+#Current issue: code allows me to import client, but not discord_token.

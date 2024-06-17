b05f399fe9006f140a3a2b77197dc6b366017958
plantainbread <119023568+plantainbread@users.noreply.github.com>
2024-06-16 20:56:10 -0700

    Update run.py

--- run.py
+++ run.py
@@ -3,4 +3,3 @@ from app.discord_bot.discord_api import client, discord_token
 if __name__ == '__main__':
     client.run(discord_token)
 
-#Current issue: code allows me to import client, but not discord_token.

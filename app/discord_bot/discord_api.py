2fc868692fb3597a5b69cf9eb635bf3c5649af8d
plantainbread <119023568+plantainbread@users.noreply.github.com>
2024-06-16 20:54:17 -0700

    Update discord_api.py

--- app/discord_bot/discord_api.py
+++ app/discord_bot/discord_api.py
@@ -1,33 +1,30 @@
 import discord
-from botAi.bot_response import gpt_response
+from app.botAi.bot_response import gpt_response
 from dotenv import load_dotenv
 import os
-#Used to access the TokenAndKey.env file and access the discord token
+
 load_dotenv()
 
-discord_token = os.getenv('DISCORD_TOKEN')
+discord_token = os.getenv("DISCORD_TOKEN_KEY")
 
 class MyClient(discord.Client):
-    #Show that bot is reading
-    async def on_read(self):
-        print(self.user,"is alert")
+
+    async def on_ready(self):
+        print("Sucessfully logged in as:", self.user)
     
     async def on_message(self, message):
         print(message.content)
         if message.author == self.user:
             return
         command, user_message=None, None
-        
-        #change the different chat commands in the list if you want to customize it.
-        for text in ['/bot','/ask','/chat']:
+    
+        for text in ['/bot','/ask','/Bo\'oh\'o\'wa\'er']:
             if message.content.startswith(text):
                 command = message.content.split(' ')[0]
-                user_message = message.replace(text, '')
+                user_message = message.content.replace(text, '')
                 print(command, user_message)
-    
-        #Don't forget to change these ones too if you want to alter the above
-        #There's probably a more efficient way to just iterate over the text list but I am going to change that later.
-        if command == '/bot' or command == '/ask' or command == '/chat':
+
+        if command == '/bot' or command == '/ask' or command == '/Bo\'oh\'o\'wa\'er':
             bot_response = gpt_response(prompt=user_message)
             await message.channel.send(f"Answer: {bot_response}")
     

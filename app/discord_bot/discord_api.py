de9ca1a5c48d26ac689becd11c28dcc872a80f41
plantainbread <119023568+plantainbread@users.noreply.github.com>
2024-06-16 21:09:50 -0700

    Update discord_api.py

--- app/discord_bot/discord_api.py
+++ app/discord_bot/discord_api.py
@@ -17,14 +17,14 @@ class MyClient(discord.Client):
         if message.author == self.user:
             return
         command, user_message=None, None
-    
-        for text in ['/bot','/ask','/Bo\'oh\'o\'wa\'er']:
+    # You may change the commands to anything you like, though you have to update the if statements below.
+        for text in ['/bot','/ask','/chat']:
             if message.content.startswith(text):
                 command = message.content.split(' ')[0]
                 user_message = message.content.replace(text, '')
                 print(command, user_message)
-
-        if command == '/bot' or command == '/ask' or command == '/Bo\'oh\'o\'wa\'er':
+    # You have to change this if you wish to alter the commands, I probably could have done this more efficiently but it is what it is.
+        if command == '/bot' or command == '/ask' or command == '/chat':
             bot_response = gpt_response(prompt=user_message)
             await message.channel.send(f"Answer: {bot_response}")
     

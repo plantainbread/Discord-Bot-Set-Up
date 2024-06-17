aa4a081fce9bd2c46680ed35e41ba6c901dff5c0
plantainbread <119023568+plantainbread@users.noreply.github.com>
2024-06-16 20:55:57 -0700

    Update bot_response.py

--- app/botAi/bot_response.py
+++ app/botAi/bot_response.py
@@ -1,17 +1,19 @@
-import openai
+from openai import OpenAI
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+api_key2 = OpenAI(
+    api_key=os.environ.get("OPENAI_API_KEY"),
+)
 
-openai.api_key = os.getenv('OPENAI_API_KEY')
-
-#configure the model used in this bot.
-#The return statement is used to only access the content key value pair because the model response in a json file format.
-#Refer to the OpenAI API reference, making requests section if you want to see it visualized.
 def gpt_response(prompt):
-        response = openai.ChatCompletion.create(
-            model='gpt-3.5-turbo', 
-            message=[{"role": "user", "content": prompt}]
-            )
-        return response.choices[0].message.content.strip()
+    chat_completion = api_key2.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": prompt,
+            }
+        ],
+        model="gpt-3.5-turbo",
+    )
+    return chat_completion.choices[0].message.content.strip()
